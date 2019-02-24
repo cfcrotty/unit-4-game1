96e57e3f016abdebfb816c9096f08306e49c2751
cfcrotty <carafeliselana@gmail.com>
2019-02-24 15:55:55 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ var defender = 0;
 var enemy1 = 0;
 var enemy2 = 0;
 var wins = 0;
+var gameOver = 0;
+var lastEnemy = 0;
 
 var characters = [];
 characters[1] = {cName:"Obi-Wan Kenobi",hp:120,ap:8,cap:10,i:"assets/images/Obi.jpg"};
@@ -34,6 +36,7 @@ $(document).ready(function () {
     });
     //on click, run codes inside the function
     $('#character1, #character2, #character3, #character4').click(function () {
+        if (gameOver) return;
         if (defender && ($(this).val() == enemy1 || $(this).val() == enemy2)) {
             defender=$(this).val();
         }
@@ -42,18 +45,30 @@ $(document).ready(function () {
                 if (mainCharacter != i) {
                     $('#character'+i).attr("disabled","disabled");
                     if ($(this).val() == i) {
+                        lastEnemy++;
                         var divToMove = $('#divCharacter'+i).detach();
                         divToMove.appendTo('#row4');
+                        $("#character"+i).toggleClass('btn-danger btn-success');
                         defender = i;
+                        $("#divCharacter"+i).toggleClass('col-md-4 col-md-12');
+                        if (lastEnemy>=3) {
+                            $("#divCharacter"+i).toggleClass('col-md-4 col-md-12');
+                        }
                     } else {
                         if (!enemy1) {
                             enemy1 = i;
+                            $("#divCharacter"+i).toggleClass('col-md-4 col-md-6');
                         } else if (!enemy2) {
                             enemy2 = i;
+                            $("#divCharacter"+i).toggleClass('col-md-4 col-md-6');
+                        } else {
+                            $("#divCharacter"+i).toggleClass('col-md-6 col-md-12');
                         }
+                        
                     }
                 }
             }
+            $("#message1").hide();
             $('#attack').removeAttr("disabled");
         }
         if (!mainCharacter && !mainCharacter && !defender) {
@@ -61,38 +76,48 @@ $(document).ready(function () {
                 if ($(this).val() != i) {
                     var divToMove = $('#divCharacter'+i).detach();
                     divToMove.appendTo('#row2');
+                    $("#character"+i).toggleClass('btn-success btn-danger');
+                    $("#divCharacter"+i).toggleClass('col-md-3 col-md-4');
                 } else {
                     $('#character'+i).attr("disabled","disabled");
                     mainCharacter = i;
+                    $("#divCharacter"+i).toggleClass('col-md-3 col-md-12');
                 }
+                $("#message1").html("Please select an opponent.");
             }
         }
     });
     $("#attack").on("click", function() {
-        characters[mainCharacter].ap += characters[mainCharacter].ap;
+        if (gameOver) return;
         characters[mainCharacter].hp-=characters[defender].cap;
         characters[defender].hp-=characters[mainCharacter].ap;
         $("#hp"+mainCharacter).text(characters[mainCharacter].hp);
         $("#hp"+defender).text(characters[defender].hp);
-        
+        $("#attackInfo1").html("You attacked "+characters[defender].cName+" for "+characters[mainCharacter].ap+" damage.");
+        $("#attackInfo2").html(characters[defender].cName+" attacked you back for "+characters[defender].cap+" damage.");
+        characters[mainCharacter].ap += characters[mainCharacter].ap;
         if (characters[mainCharacter].hp<=0) {
+            gameOver++;
             $('#attack').attr("disabled","disabled");
             $('#restart').removeAttr("disabled");
             $("#message").html("Game Over! You lost.");
             alert("Game Over. You lost.");
             return;
         } else if (characters[defender].hp<=0) {
+            $("#message1").html("Please select an opponent.");
+            $("#message1").show();
             wins++;
             $('#divCharacter'+defender).remove();
             $('#character'+enemy1).removeAttr("disabled");
             $('#character'+enemy2).removeAttr("disabled");
             $('#attack').attr("disabled","disabled");
-            alert(wins);
             if (wins>=3) {
+                gameOver++;
                 $('#restart').removeAttr("disabled");
                 $("#message").html("Game Over! You won.");
                 alert("Game Over! You won.");
             }
+            return;
         }
         
     });

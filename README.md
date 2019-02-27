7c521b961f8dba5c03bca3dd4077855a1d6d99f2
cfcrotty <carafeliselana@gmail.com>
2019-02-26 19:54:27 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # unit-4-game1
-unit-4-game Homework
+Star Wars Game Homework by Cara Felise
+
+
+This Star Wars Game is like an RPG game where the player assume the roles of characters in a fictional setting. When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game. The player chooses an opponent by clicking on an enemy's picture. Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture. The player must then defeat all of the remaining fighters. Users just have to restart the game when game is over.
+
+The project is useful because it provides an example of languages/technologies I learned and show what I can do as a developer. It is using JQuery, some HTML and CSS.
+
+For questions or concerns, please go to my website at carafelise.com or send an email at admin@carafelise.com. I maintain and developed this project.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,30 @@ var enemy2 = 0;
 var wins = 0;
 var gameOver = 0;
 var lastEnemy = 0;
+var mainAP = 0;
 
-var characters = [];
+var characters = []; //hp=health points, ap=attack power, cap=counter attack power, i=image
 characters[1] = {cName:"Obi-Wan Kenobi",hp:120,ap:8,cap:10,i:"assets/images/Obi.jpg"};
-characters[2] = {cName:"Luke Skywalker",hp:100,ap:10,cap:15,i:"assets/images/Luke.jpg"};
-characters[3] = {cName:"Darth Sidious",hp:150,ap:12,cap:20,i:"assets/images/Sidious.jpg"};
-characters[4] = {cName:"Darth Maul",hp:180,ap:14,cap:25,i:"assets/images/Maul.jpeg"};
+characters[2] = {cName:"Luke Skywalker",hp:100,ap:12,cap:5,i:"assets/images/Luke.jpg"};
+characters[3] = {cName:"Darth Sidious",hp:150,ap:4,cap:20,i:"assets/images/Sidious.jpg"};
+characters[4] = {cName:"Darth Maul",hp:180,ap:2,cap:25,i:"assets/images/Maul.jpeg"};
 //after the document is ready, load these
 $(document).ready(function () {
-    $("#info1").html("&nbsp;"+characters[1].cName+"<br><img class='image' src='"+characters[1].i+"'>&nbsp;");
-    $("#hp1").html("&nbsp;"+characters[1].hp+"&nbsp;");
-    $("#info2").html("&nbsp;"+characters[2].cName+"<br><img class='image' src='"+characters[2].i+"'>&nbsp;");
-    $("#hp2").html("&nbsp;"+characters[2].hp+"&nbsp;");
-    $("#info3").html("&nbsp;"+characters[3].cName+"<br><img class='image' src='"+characters[3].i+"'>&nbsp;");
-    $("#hp3").html("&nbsp;"+characters[3].hp+"&nbsp;");
-    $("#info4").html("&nbsp;"+characters[4].cName+"<br><img class='image' src='"+characters[4].i+"'>&nbsp;");
-    $("#hp4").html("&nbsp;"+characters[4].hp+"&nbsp;");
+    
+
+    function loadSong(flag) {
+        var audioToPlay = "assets/audio/LightsaberOn.mp3";
+        if (flag==0) audioToPlay = "assets/audio/SadTrombone.mp3";
+        else if (flag==1) audioToPlay = "assets/audio/Applause.mp3";
+        var audio = new Audio(audioToPlay);
+        audio.play();
+    }
+    
+    //insert character information like name and health points
+    for (var i=1;i<=4;i++) {
+        $("#info"+i).html("&nbsp;"+characters[i].cName+"<br><img class='image' src='"+characters[i].i+"'>&nbsp;");
+        $("#hp"+i).html("&nbsp;"+characters[i].hp+"&nbsp;");
+    }
     //function to restart game
     $("#restart").on("click", function() {
         location.href = "index.html";
@@ -34,7 +42,7 @@ $(document).ready(function () {
     $("#close").on("click", function() {
         $("#cover, #aboutDiv").hide();
     });
-    //on click, run codes inside the function
+    //on click of each character, run codes inside the function
     $('#character1, #character2, #character3, #character4').click(function () {
         if (gameOver) return;
         if (defender && ($(this).val() == enemy1 || $(this).val() == enemy2)) {
@@ -82,43 +90,52 @@ $(document).ready(function () {
                     $('#character'+i).attr("disabled","disabled");
                     mainCharacter = i;
                     $("#divCharacter"+i).toggleClass('col-md-3 col-md-12');
+                    $("#message1").html("Please select an opponent.");
                 }
-                $("#message1").html("Please select an opponent.");
             }
         }
     });
+    //on click of attack button, run codes inside the function
     $("#attack").on("click", function() {
         if (gameOver) return;
-        characters[mainCharacter].hp-=characters[defender].cap;
-        characters[defender].hp-=characters[mainCharacter].ap;
-        $("#hp"+mainCharacter).text(characters[mainCharacter].hp);
-        $("#hp"+defender).text(characters[defender].hp);
-        $("#attackInfo1").html("You attacked "+characters[defender].cName+" for "+characters[mainCharacter].ap+" damage.");
-        $("#attackInfo2").html(characters[defender].cName+" attacked you back for "+characters[defender].cap+" damage.");
-        characters[mainCharacter].ap += characters[mainCharacter].ap;
-        if (characters[mainCharacter].hp<=0) {
-            gameOver++;
-            $('#attack').attr("disabled","disabled");
-            $('#restart').removeAttr("disabled");
-            $("#message").html("Game Over! You lost.");
-            alert("Game Over. You lost.");
-            return;
-        } else if (characters[defender].hp<=0) {
+        mainAP += characters[mainCharacter].ap;
+        characters[defender].hp-=mainAP;
+        if (characters[defender].hp<=0) {
             $("#message1").html("Please select an opponent.");
             $("#message1").show();
             wins++;
+            $("#attackInfo1").hide();
+            $("#attackInfo2").hide();
             $('#divCharacter'+defender).remove();
             $('#character'+enemy1).removeAttr("disabled");
             $('#character'+enemy2).removeAttr("disabled");
             $('#attack').attr("disabled","disabled");
             if (wins>=3) {
+                loadSong (1);
                 gameOver++;
+                $("#message1").hide();
                 $('#restart').removeAttr("disabled");
                 $("#message").html("Game Over! You won.");
                 alert("Game Over! You won.");
             }
             return;
+        } else if (characters[mainCharacter].hp<=0) {
+            loadSong (0);
+            gameOver++;
+            $("#message1").hide();
+            $('#attack').attr("disabled","disabled");
+            $('#restart').removeAttr("disabled");
+            $("#message").html("Game Over! You lost.");
+            alert("Game Over. You lost.");
+            return;
         }
-        
+        loadSong();
+        characters[mainCharacter].hp-=characters[defender].cap;
+        $("#hp"+mainCharacter).text(characters[mainCharacter].hp);
+        $("#hp"+defender).text(characters[defender].hp);
+        $("#attackInfo1").html("You attacked "+characters[defender].cName+" for "+mainAP+" damage.");
+        $("#attackInfo2").html(characters[defender].cName+" attacked you back for "+characters[defender].cap+" damage.");
+        $("#attackInfo1").show();
+        $("#attackInfo2").show();
     });
 });
